deee1e61a39c5daf4e2afb07ab66cf6e258ad87a
Karina <Karishka.1996@mail.ru>
2023-01-30 13:53:17 +0400

    задача номер 1

---
+++ 001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа: " + stringNumber[1]);
